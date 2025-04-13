9db4bf200dfbcdd80ecdc11cf43eda7580b69855
PearCoding <pearcoding@gmail.com>
2025-04-13 13:54:18 +0200

    added repeat border as the new default

--- renderer/texture.h
+++ renderer/texture.h
@@ -50,14 +50,29 @@ inline RA_FUNCTION TextureTexel map_uv_to_image_pixel(vec2 uv, const Texture& te
     };
 }
 
+inline RA_FUNCTION int handle_clamp_border(int i, int s) {
+    return clamp(i, 0, s - 1);
+}
+
+inline RA_FUNCTION int handle_repeat_border(int i, int s) {
+    int t = i % s;
+    return t < 0 ?  t + s : t;
+}
+
 inline RA_FUNCTION vec3 lookup_texture(vec2 uv, const Texture& tex) {
     const auto texel = map_uv_to_image_pixel(uv, tex);
 
     // Clamp border
-    int x0 = clamp(texel.ix + 0, 0, tex.width  - 1);
-    int y0 = clamp(texel.iy + 0, 0, tex.height - 1);
-    int x1 = clamp(texel.ix + 1, 0, tex.width  - 1);
-    int y1 = clamp(texel.iy + 1, 0, tex.height - 1);
+    // int x0 = handle_clamp_border(texel.ix + 0, tex.width );
+    // int y0 = handle_clamp_border(texel.iy + 0, tex.height);
+    // int x1 = handle_clamp_border(texel.ix + 1, tex.width );
+    // int y1 = handle_clamp_border(texel.iy + 1, tex.height);
+
+    // Repeat border
+    int x0 = handle_repeat_border(texel.ix + 0, tex.width );
+    int y0 = handle_repeat_border(texel.iy + 0, tex.height);
+    int x1 = handle_repeat_border(texel.ix + 1, tex.width );
+    int y1 = handle_repeat_border(texel.iy + 1, tex.height);
 
     // Bilinear filtering
     vec3 p00 = fetch_texture_unsafe(x0, y0, tex);
